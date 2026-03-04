fb1d7b725c3e894789058f612e3278d877070c12
Anna Bowen <59578289+anbowe@users.noreply.github.com>
2026-03-03 17:38:18 -0800

    Update cite.py to only draw from sources

--- _cite/cite.py
+++ _cite/cite.py
@@ -29,7 +29,8 @@ log("Compiling sources")
 sources = []
 
 # in-order list of plugins to run
-plugins = ["google-scholar", "pubmed", "orcid", "sources"]
+# plugins = ["google-scholar", "pubmed", "orcid", "sources"]
+plugins = ["sources"]
 
 # loop through plugins
 for plugin in plugins:
